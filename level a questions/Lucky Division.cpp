62bc19f2b7865b6e12faf33aa1d96ca85c3e4d22
Firdaus Jawed <85278666+FirdausJawed@users.noreply.github.com>
2021-11-30 23:20:37 +0530

    added another question

--- level a questions/Lucky Division.cpp
+++ level a questions/Lucky Division.cpp
@@ -53,8 +53,11 @@ double eps = 1e-12;
  
 
 void solve(){
-    int arr{};
-
+   int n;
+  cin >> n;
+  cout <<  (n % 4 == 0 || n % 7 == 0 || n % 47 == 0 || n % 74 == 0 || 
+    n % 447 == 0 || n % 474 == 0 || n % 744 == 0 || n % 747 == 0 ||
+    n % 477 == 0 || n % 744 == 0 ? "YES" : "NO");
 }
 int main()
 {
