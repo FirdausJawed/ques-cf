5ca865dbdde28f8ad602cbfe27865039f3d9a075
Firdaus Jawed <85278666+FirdausJawed@users.noreply.github.com>
2021-12-24 00:31:55 +0530

    .

---
+++ C_Not_Adjacent_Matrix.cpp
@@ -0,0 +1,141 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#include <complex>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <list>
+#include <chrono>
+#include <random>
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <iomanip>
+#include <fstream>
+
+using namespace std;
+typedef unsigned long long ull;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> p32;
+typedef pair<ll, ll> p64;
+typedef pair<double, double> pdd;
+typedef vector<ll> v64;
+typedef vector<int> v32;
+typedef vector<vector<int>> vv32;
+typedef vector<vector<ll>> vv64;
+typedef vector<vector<p64>> vvp64;
+typedef vector<p64> vp64;
+typedef vector<p32> vp32;
+ll MOD = 1000000007;
+double eps = 1e-12;
+#define forn(i, n) for (ll i = 0; i < n; i++)
+#define forsn(i, s, e) for (ll i = s; i < e; i++)
+#define rforn(i, s) for (ll i = s; i >= 0; i--)
+#define rforsn(i, s, e) for (ll i = s; i >= e; i--)
+#define ln "\n"
+#define dbg(x) cout << #x << " = " << x << ln
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define INF 2e18
+#define fast_cin()                    \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+#define al(arr, n) arr, arr + n
+#define sz(x) ((ll)(x).size())
+#define maxi 1001
+ll arr[maxi][maxi];
+
+void solve()
+{
+    int n;
+    cin >> n;
+    if (n == 2)
+    {
+        cout << -1 << ln;
+        return;
+    }
+    ll size = n * n;
+    int val[size];
+    forn(i, size)
+    {
+        val[i] = i + 1;
+    }
+    if (n % 2 == 0)
+    {
+        ll a = 0, b = size / 2, k = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                if (k == 0)
+                {
+                    arr[i][j] = val[a++];
+                    k = 1;
+                }
+                else
+                {
+                    arr[i][j] = val[b++];
+                    k = 0;
+                }
+                
+            }
+        }
+    }
+    else
+    {
+        ll a = 0, b = size / 2 + 1, lastval = size / 2, k = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                if (k == 0)
+                {
+                    arr[i][j] = val[a++];
+                    k = 1;
+                }
+                else
+                {
+                    arr[i][j] = val[b++];
+                    k = 0;
+                }
+                if (i == n && j == n)
+                {
+                    arr[i][j] = val[lastval];
+                }
+              
+            }
+        }
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << ln;
+    }
+}
+
+int main()
+{
+    fast_cin();
+    ll t;
+    cin >> t;
+    for (int it = 1; it <= t; it++)
+    {
+        solve();
+    }
+    return 0;
+}
