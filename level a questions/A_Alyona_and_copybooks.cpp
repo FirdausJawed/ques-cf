1addf902df12a0f74d76118521a2b08d955790a0
Firdaus Jawed <85278666+FirdausJawed@users.noreply.github.com>
2021-12-25 14:10:03 +0530

    .

--- level a questions/A_Alyona_and_copybooks.cpp
+++ level a questions/A_Alyona_and_copybooks.cpp
@@ -1,7 +1,7 @@
 #pragma GCC optimize("Ofast")
 #pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
 #pragma GCC optimize("unroll-loops")
-#include <bits/stdc++.h> 
+#include <bits/stdc++.h>
 #include <complex>
 #include <queue>
 #include <set>
@@ -19,69 +19,76 @@
 #include <stack>
 #include <iomanip>
 #include <fstream>
- 
+
 using namespace std;
-typedef unsigned long long ull; 
+typedef unsigned long long ull;
 typedef long long ll;
 typedef long double ld;
-typedef pair<int,int> p32;
-typedef pair<ll,ll> p64;
-typedef pair<double,double> pdd;
+typedef pair<int, int> p32;
+typedef pair<ll, ll> p64;
+typedef pair<double, double> pdd;
 typedef vector<ll> v64;
 typedef vector<int> v32;
-typedef vector<vector<int> > vv32;
-typedef vector<vector<ll> > vv64;
-typedef vector<vector<p64> > vvp64;
+typedef vector<vector<int>> vv32;
+typedef vector<vector<ll>> vv64;
+typedef vector<vector<p64>> vvp64;
 typedef vector<p64> vp64;
 typedef vector<p32> vp32;
 ll MOD = 1000000007;
 double eps = 1e-12;
-#define forn(i,n) for(ll i = 0; i < n; i++)
-#define forsn(i,s,e) for(ll i = s; i < e; i++)
-#define rforn(i,s) for(ll i = s; i >= 0; i--)
-#define rforsn(i,s,e) for(ll i = s; i >= e; i--)
+#define forn(i, n) for (ll i = 0; i < n; i++)
+#define forsn(i, s, e) for (ll i = s; i < e; i++)
+#define rforn(i, s) for (ll i = s; i >= 0; i--)
+#define rforsn(i, s, e) for (ll i = s; i >= e; i--)
 #define ln "\n"
-#define dbg(x) cout<<#x<<" = "<<x<<ln
+#define dbg(x) cout << #x << " = " << x << ln
 #define mp make_pair
 #define pb push_back
 #define fi first
 #define se second
 #define INF 2e18
-#define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define fast_cin()                    \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
 #define all(x) (x).begin(), (x).end()
-#define al(arr,n) arr,arr+n
+#define al(arr, n) arr, arr + n
 #define sz(x) ((ll)(x).size())
- 
 
 void solve()
-{ 
- ll n,a,b,c;
- cin>>n>>a>>b>>c;
- 
- if (n%4==0)
- {
-     cout<<0<<ln;
- }
- else if (n%4==1)
- {
-     cout<<min({3*a,a+c,c})<<ln;
- }
- else if (n%4==2)
- {
-      cout<<min({2*a,c*2,b})<<ln;
- }
- else{
-      cout<<min({a,b+c,3*c})<<ln;
- }
- 
+{
+    ll n, a, b, c;
+    cin >> n >> a >> b >> c;
+    ll k = n % 4;
+    if (k == 0)
+    {
+        cout << "0" << ln;
+        return;
+    }
+    if (k == 1)
+    {
+        cout << min({3 * a, c, b + a}) << ln;
+        return;
+    }
+    if (k == 2)
+    {
+        cout << min({2 * c, b, 2 * a}) << ln;
+        return;
+    }
+    if (k == 3)
+    {
+        cout << min({3 * c, b + c, a}) << ln;
+        return;
+    }
 }
+
 int main()
 {
- fast_cin();
- ll t;
- cin >> t;
- //for(int it=1;it<=t;it++) {
- solve();
- //}
- return 0;
+    fast_cin();
+    // ll t=1;
+    // cin >> t;
+    //for(int it=1;it<=t;it++) {
+    solve();
+    //}
+    return 0;
 }
