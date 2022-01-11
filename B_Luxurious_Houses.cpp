ee397d28fccb63a805cb435039785625bce091c6
Firdaus Jawed <85278666+FirdausJawed@users.noreply.github.com>
2022-01-11 18:22:02 +0530

    .

--- B_Luxurious_Houses.cpp
+++ B_Luxurious_Houses.cpp
@@ -1,7 +1,7 @@
 #pragma GCC optimize("Ofast")
 #pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
 #pragma GCC optimize("unroll-loops")
-#include <bits/stdc++.h> 
+#include <bits/stdc++.h>
 #include <complex>
 #include <queue>
 #include <set>
@@ -19,58 +19,94 @@
 #include <stack>
 #include <iomanip>
 #include <fstream>
- 
+
 using namespace std;
-typedef unsigned long long ull; 
+typedef unsigned long long ull;
 typedef long long ll;
 typedef long double ld;
-typedef pair<int,int> p32;
-typedef pair<ll,ll> p64;
-typedef pair<double,double> pdd;
+typedef pair<int, int> p32;
+typedef pair<ll, ll> p64;
+typedef pair<double, double> pdd;
 typedef vector<ll> v64;
 typedef vector<int> v32;
-typedef vector<vector<int> > vv32;
-typedef vector<vector<ll> > vv64;
-typedef vector<vector<p64> > vvp64;
+typedef vector<vector<int>> vv32;
+typedef vector<vector<ll>> vv64;
+typedef vector<vector<p64>> vvp64;
 typedef vector<p64> vp64;
 typedef vector<p32> vp32;
 ll MOD = 1000000007;
 double eps = 1e-12;
-#define forn(i,n) for(ll i = 0; i < n; i++)
-#define forsn(i,s,e) for(ll i = s; i < e; i++)
-#define rforn(i,s) for(ll i = s; i >= 0; i--)
-#define rforsn(i,s,e) for(ll i = s; i >= e; i--)
+#define forn(i, n) for (ll i = 0; i < n; i++)
+#define forsn(i, s, e) for (ll i = s; i < e; i++)
+#define rforn(i, s) for (ll i = s; i >= 0; i--)
+#define rforsn(i, s, e) for (ll i = s; i >= e; i--)
 #define ln "\n"
-#define dbg(x) cout<<#x<<" = "<<x<<ln
+#define dbg(x) cout << #x << " = " << x << ln
 #define mp make_pair
 #define pb push_back
 #define fi first
 #define se second
 #define INF 2e18
-#define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define fast_cin()                    \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
 #define all(x) (x).begin(), (x).end()
-#define al(arr,n) arr,arr+n
+#define al(arr, n) arr, arr + n
 #define sz(x) ((ll)(x).size())
- 
 
 void solve()
-{ 
- int n;
- cin>>n;
- int arr[n];
- forn(i,n)
- {
- cin>>arr[i];
- }
- 
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    forn(i, n)
+    {
+        cin >> arr[i];
+    }
+
+    v64 v;
+    int leader = arr[n - 1];
+    v.pb(0);
+
+    for (int i = n - 2; i >= 0; i--)
+    {
+        if (arr[i] > leader)
+        {
+            leader = arr[i];
+        }
+        int x = leader;
+        v.pb(x);
+    }
+    int t = 0;
+
+    for (int i = n - 1; i >= 0; i--)
+    {
+        if (i == n - 1)
+        {
+            cout << 0 << " ";
+            continue;
+        }
+        if (arr[i] == leader && t == 0)
+        {
+            t++;
+            cout << 0 << " ";
+        }
+        else
+        {
+            cout << v[i] - arr[i] + 1 << " ";
+        }
+    }
+    cout << ln;
 }
 int main()
 {
- fast_cin();
- ll t;
- cin >> t;
- for(int it=1;it<=t;it++) {
- solve();
- }
- return 0;
+    fast_cin();
+    ll t = 1;
+    //cin >> t;
+    for (int it = 1; it <= t; it++)
+    {
+        solve();
+    }
+    return 0;
 }
