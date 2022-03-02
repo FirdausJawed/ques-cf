37a7f64831a161d0369d38d5788c1012887d2df5
Firdaus Jawed <85278666+FirdausJawed@users.noreply.github.com>
2022-03-02 23:02:43 +0530

    .

--- c.cpp
+++ c.cpp
@@ -1,7 +1,7 @@
 #pragma GCC optimize("Ofast")
 #pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
 #pragma GCC optimize("unroll-loops")
-#include <bits/stdc++.h>
+#include <bits/stdc++.h> 
 #include <complex>
 #include <queue>
 #include <set>
@@ -19,201 +19,160 @@
 #include <stack>
 #include <iomanip>
 #include <fstream>
-
+ 
 using namespace std;
-typedef unsigned long long ull;
+typedef unsigned long long ull; 
 typedef long long ll;
 typedef long double ld;
-typedef pair<int, int> p32;
-typedef pair<ll, ll> p64;
-typedef pair<double, double> pdd;
+typedef pair<int,int> p32;
+typedef pair<ll,ll> p64;
+typedef pair<double,double> pdd;
 typedef vector<ll> v64;
 typedef vector<int> v32;
-typedef vector<vector<int>> vv32;
-typedef vector<vector<ll>> vv64;
-typedef vector<vector<p64>> vvp64;
+typedef vector<vector<int> > vv32;
+typedef vector<vector<ll> > vv64;
+typedef vector<vector<p64> > vvp64;
 typedef vector<p64> vp64;
 typedef vector<p32> vp32;
 ll MOD = 1000000007;
 double eps = 1e-12;
-#define forn(i, n) for (ll i = 0; i < n; i++)
-#define forsn(i, s, e) for (ll i = s; i < e; i++)
-#define rforn(i, s) for (ll i = s; i >= 0; i--)
-#define rforsn(i, s, e) for (ll i = s; i >= e; i--)
+#define forn(i,n) for(ll i = 0; i < n; i++)
+#define forsn(i,s,e) for(ll i = s; i < e; i++)
+#define rforn(i,s) for(ll i = s; i >= 0; i--)
+#define rforsn(i,s,e) for(ll i = s; i >= e; i--)
 #define ln "\n"
-#define dbg(x) cout << #x << " = " << x << ln
+#define dbg(x) cout<<#x<<" = "<<x<<ln
 #define mp make_pair
 #define pb push_back
 #define fi first
 #define se second
 #define INF 2e18
-#define fast_cin()                    \
-    ios_base::sync_with_stdio(false); \
-    cin.tie(NULL);                    \
-    cout.tie(NULL)
+#define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
 #define all(x) (x).begin(), (x).end()
-#define al(arr, n) arr, arr + n
+#define al(arr,n) arr,arr+n
 #define sz(x) ((ll)(x).size())
-
-// function for prime factorization
-vector<pair<ll, ll>> pf(ll n)
-{
-    vector<pair<ll, ll>> prime;
-    for (int i = 2; i <= sqrt(n); i++)
-    {
-        if (n % i == 0)
-        {
-            int count = 0;
-            while (n % i == 0)
-            {
-                count++;
-                n = n / i;
-            }
-            prime.pb(mp(i, count));
-        }
-    }
-    if (n > 1)
-    {
-        prime.pb(mp(n, 1));
-    }
-    return prime;
+ 
+//function for prime factorization
+vector<pair<ll, ll>> pf(ll n)  {      vector<pair<ll, ll>> prime;      for (int i = 2; i <= sqrt(n); i++)      {          if (n % i == 0)          {              int count = 0;              while (n % i == 0)              {                  count++;                  n = n / i;              }              prime.pb(mp(i, count));          }      }      if (n > 1)      {          prime.pb(mp(n, 1));      }      return prime;  } 
+ 
+//sum of digits of a number
+ll sumofno(ll n)  {      ll sum = 0;      while (n != 0)      {          sum += n % 10;          n = n / 10;      }      return sum;  } 
+ 
+ 
+//modular exponentiation 
+long long modpow(long long val, long long deg, long long mod) {
+if (!deg)
+return 1 % mod;
+if (deg & 1)
+return modpow(val, deg - 1, mod) * val % mod;
+long long res = modpow(val, deg >> 1, mod);
+return (res * res) % mod;
 }
-
-// sum of digits of a number
-ll sumofno(ll n)
-{
-    ll sum = 0;
-    while (n != 0)
-    {
-        sum += n % 10;
-        n = n / 10;
-    }
-    return sum;
-}
-
-// modular exponentiation
-long long modpow(long long val, long long deg, long long mod)
-{
-    if (!deg)
-        return 1 % mod;
-    if (deg & 1)
-        return modpow(val, deg - 1, mod) * val % mod;
-    long long res = modpow(val, deg >> 1, mod);
-    return (res * res) % mod;
-}
-
+ 
 const int N = 1e6 + 100;
 long long fact[N];
 // initialise the factorial
-void initfact()
+void initfact(){
+fact[0] = 1;
+for (int i = 1; i < N; i++)
 {
-    fact[0] = 1;
-    for (int i = 1; i < N; i++)
-    {
-        fact[i] = (fact[i - 1] * i);
-        fact[i] %= MOD;
-    }
-}
-
-// formula for c
+fact[i] = (fact[i - 1] * i);
+fact[i] %= MOD;
+}}
+ 
+//formula for c 
 ll C(ll n, ll i)
 {
-    ll res = fact[n];
-    ll div = fact[n - i] * fact[i];
-    div %= MOD;
-    div = modpow(div, MOD - 2, MOD);
-    return (res * div) % MOD;
+ll res = fact[n];
+ll div = fact[n - i] * fact[i];
+div %= MOD;
+div = modpow(div, MOD - 2, MOD);
+return (res * div) % MOD;
 }
-
-// function for fast expo
-ll fastexpo(ll a, ll b)
-{
-    if (b == 0)
-    {
-        return 1;
-    }
-    if (a == 0)
-    {
-        return 0;
-    }
-    ll y = fastexpo(a, b / 2);
-    if (b % 2 == 0)
-    {
-        return y * y;
-    }
-    else
-    {
-        return a * y * y;
-    }
-}
-
+ 
+//function for fast expo 
+ll fastexpo(ll a, ll b)  {      if (b == 0)      {          return 1;      }      if (a == 0)      {          return 0;      }        ll y = fastexpo(a, b / 2);        if (b % 2 == 0)      {          return y * y;      }      else      {          return a * y * y;      }  }   
+ 
 ll popcount(ll n)
 {
-    ll c = 0;
-    for (; n; ++c)
-        n &= n - 1;
-    return c;
+ll c = 0;
+for (; n; ++c)
+n &= n - 1;
+return c;
 }
-
+ 
 ll ce(ll x, ll y)
 {
-    ll res = x / y;
-    if (x % y != 0)
-    {
-        res++;
-    }
-    return res;
+ll res = x / y;
+if (x % y != 0)
+{
+res++;
 }
-
+return res;
+}
+ 
 bool pow2(ll x)
 {
-    ll res = x & (x - 1);
-    if (res == 0)
-    {
-        return true;
-    }
-    return false;
+ll res = x & (x - 1);
+if (res == 0)
+{
+return true;
+}
+return false;
+}
+
+void dfs(ll n, ll viss[], vector<ll> *adj)
+{
+    viss[n] = 1;
+    for (auto i : adj[vertex])
+        if (viss[i] == 0)
+            dfs(i, viss, adj);
 }
 
 void solve()
 {
-    ll n, w;
-    cin >> n >> w;
-    ll arr[n];
-    forn(i, n)
+    ll n, m;
+    cin >> n >> m;
+    vector<ll> adj[n];
+    for (ll i = 0; i < m; i++)
     {
-        cin >> arr[i];
+        ll x, y;
+        cin >> x >> y;
+        adj[x - 1].push_back(y - 1);
+        adj[y - 1].push_back(x - 1);
     }
-
-    sort(al(arr, n), greater<ll>());
-    ll sum = 0, holiday = 0, cnt = 0;
-
-    forn(i, n)
+    ll vis[n] = {0};
+    dfs(0, vis, adj);
+    ll ans = 0;
+    while (vis[n - 1] == 0)
     {
-        if (sum >= w)
-        {
-            break;
-        }
-        cnt++;
-        sum += arr[i];
+        ll vis2[n] = {0};
+        for (ll i = 0; i < n; i++)
+            if (vis[i])
+            {
+                if (i + 1 < n)
+                    dfs(i + 1, vis2, adj);
+                if (i - 1 >= 0)
+                    dfs(i - 1, vis2, adj);
+            }
+        for (ll i = 0; i < n; i++)
+            vis[i] = (vis[i] | vis2[i]);
+        ans++;
     }
-
-    holiday = n - cnt;
-    cout << holiday << ln;
+    cout << ans << endl;
 }
 int main()
 {
-    fast_cin();
-    ll t;
-    cin >> t;
-    for (int it = 1; it <= t; it++)
-    {
-        solve();
-    }
-    return 0;
+ fast_cin();
+ ll t;
+ cin >> t;
+ for(int it=1;it<=t;it++) {
+ solve();
+ }
+ return 0;
 }
-
-/*
+ 
+/* 
 1. Check borderline constraints. Can a variable you are dividing by be 0?
 2. Use ll while using bitshifts
 3. Do not erase from set while iterating it
@@ -221,4 +180,5 @@ int main()
 5. Read the task carefully, is something unique, sorted, adjacent, guaranteed??
 6. DO NOT use if(!mp[x]) if you want to iterate the map later
 7. Are you using i in all loops? Are the i's conflicting?
-*/
+*/ 
+ 
