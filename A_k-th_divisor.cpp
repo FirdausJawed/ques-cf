48bc1a3cd9ecd538f5d9d7ea4bcfdf85f2730aa0
Firdaus Jawed <85278666+FirdausJawed@users.noreply.github.com>
2022-03-03 15:01:11 +0530

    added another codeforces problem

--- A_k-th_divisor.cpp
+++ A_k-th_divisor.cpp
@@ -56,140 +56,47 @@ double eps = 1e-12;
 #define sz(x) ((ll)(x).size())
 
 // function for prime factorization
-vector<pair<ll, ll>> pf(ll n)
+vector<ll> pf(ll n)
 {
-    vector<pair<ll, ll>> prime;
-    for (int i = 2; i <= sqrt(n); i++)
+    vector<ll> prime;
+    for (int i = 1; i <= sqrt(n); i++)
     {
         if (n % i == 0)
         {
-            int count = 0;
-            while (n % i == 0)
+            if (n/i==i)
             {
-                count++;
-                n = n / i;
+                prime.pb(i);
             }
-            prime.pb(mp(i, count));
+            else{
+                prime.pb(n / i);
+                prime.pb(i);
+            }
         }
-    }
-    if (n > 1)
-    {
-        prime.pb(mp(n, 1));
     }
     return prime;
 }
 
-// sum of digits of a number
-ll sumofno(ll n)
-{
-    ll sum = 0;
-    while (n != 0)
-    {
-        sum += n % 10;
-        n = n / 10;
-    }
-    return sum;
-}
-
-// modular exponentiation
-long long modpow(long long val, long long deg, long long mod)
-{
-    if (!deg)
-        return 1 % mod;
-    if (deg & 1)
-        return modpow(val, deg - 1, mod) * val % mod;
-    long long res = modpow(val, deg >> 1, mod);
-    return (res * res) % mod;
-}
-
-const int N = 1e6 + 100;
-long long fact[N];
-// initialise the factorial
-void initfact()
-{
-    fact[0] = 1;
-    for (int i = 1; i < N; i++)
-    {
-        fact[i] = (fact[i - 1] * i);
-        fact[i] %= MOD;
-    }
-}
-
-// formula for c
-ll C(ll n, ll i)
-{
-    ll res = fact[n];
-    ll div = fact[n - i] * fact[i];
-    div %= MOD;
-    div = modpow(div, MOD - 2, MOD);
-    return (res * div) % MOD;
-}
-
-// function for fast expo
-ll fastexpo(ll a, ll b)
-{
-    if (b == 0)
-    {
-        return 1;
-    }
-    if (a == 0)
-    {
-        return 0;
-    }
-    ll y = fastexpo(a, b / 2);
-    if (b % 2 == 0)
-    {
-        return y * y;
-    }
-    else
-    {
-        return a * y * y;
-    }
-}
-
-ll popcount(ll n)
-{
-    ll c = 0;
-    for (; n; ++c)
-        n &= n - 1;
-    return c;
-}
-
-ll ce(ll x, ll y)
-{
-    ll res = x / y;
-    if (x % y != 0)
-    {
-        res++;
-    }
-    return res;
-}
-
-bool pow2(ll x)
-{
-    ll res = x & (x - 1);
-    if (res == 0)
-    {
-        return true;
-    }
-    return false;
-}
-
 void solve()
 {
-    ll n;
-    cin >> n;
-    ll arr[n];
-    forn(i, n)
+    ll n,k;
+    cin >> n>>k;
+
+    v64 v = pf(n);
+    sort(all(v));
+
+    if (v.size()<k)
     {
-        cin >> arr[i];
+        cout << -1 << ln;
+    }
+    else{
+        cout << v[k - 1] << ln;
     }
 }
 int main()
 {
     fast_cin();
-    ll t;
-    cin >> t;
+    ll t=1;
+    // cin >> t;
     for (int it = 1; it <= t; it++)
     {
         solve();
