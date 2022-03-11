23dfee3f0fdc8abe18aac1100024c64466dd8ff7
Firdaus Jawed <85278666+FirdausJawed@users.noreply.github.com>
2022-03-11 19:32:31 +0530

    added another question from codeforces

--- c.c
+++ c.c
@@ -1,16 +1,76 @@
 #include <stdio.h>
-
-void swap(int *a, int *b)
+#include <conio.h>
+struct complex
 {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+    float real, imag;
+} a, b, c;
+struct complex read(void);
+void write(struct complex);
+struct complex add(struct complex, struct complex);
+struct complex sub(struct complex, struct complex);
+struct complex mul(struct complex, struct complex);
+struct complex div(struct complex, struct complex);
+void main()
+{
+    printf("Enter the 1st complex number --> \n ");
+    a = read();
+    write(a);
+    printf("Enter the 2nd complex number --> \n");
+    b = read();
+    write(b);
+    printf("Addition\n ");
+    c = add(a, b) write(c);
+    printf("Substraction\n ");
+    c = sub(a, b);
+    write(c);
+    printf("Multiplication\n");
+    c = mul(a, b);
+    write(c);
+    printf("Division\n");
+    c = div(a, b);
+    write(c);
+    getch();
 }
-
-int main()
+struct complex read(void)
 {
-    int a = 10, b = 20;
-    swap(&a, &b);
-    printf("%d %d", a, b);
-    return 0;
+    struct complex t;
+    printf("Enter the real part: ");
+    scanf("%f", &t.real);
+    printf("Enter the imaginary part: ");
+    scanf("%f", &t.imag);
+    return t;
+}
+void write(struct complex a)
+{
+    printf("Complex number is ");
+    printf(" %.1f + i %.1f", a.real, a.imag);
+    printf("\n");
+}
+struct complex add(struct complex p, struct complex q)
+{
+    struct complex t;
+    t.real = (p.real + q.real);
+    t.imag = (p.imag + q.imag);
+    return t;
+}
+struct complex sub(struct complex p, struct complex q)
+{
+    struct complex t;
+    t.real = (p.real - q.real);
+    t.imag = (p.imag - q.imag);
+    return t;
+}
+truct complex mul(struct complex p, struct complex q)
+{
+    struct complex t;
+    t.real = (p.real * q.real) - (p.imag * q.imag);
+    t.imag = (p.real * q.imag) + (p.imag * q.real);
+    return t;
+}
+struct complex div(struct complex p, struct complex q)
+{
+    struct complex t;
+    t.real = ((p.imag * q.real) - (p.real * q.imag)) / ((q.real * q.real) + (q.imag * q.imag));
+    t.imag = ((p.real * q.real) + (p.imag * q.imag)) / ((q.real * q.real) + (q.imag * q.imag));
+    return (t);
 }
